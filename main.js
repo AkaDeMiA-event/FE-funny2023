48a21edc7fa3a8af07ba577d1076b8deeb53d36b
ryosuke12343 <ryosuke313047@gmail.com>
2023-05-09 14:33:04 +0900

    修正

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //ここに記述してもらう
+const daytime = document.querySelector("")
+const car = document.querySelector("")
+
+
+document.addEventListener("keyup", (e)=>{
+    // ~~~ここに書く~~~
+
+
+    // ~~~~~~~~~~~~~
+}, false)
